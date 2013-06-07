b670646d131556ba8675be1235593d1bccaee4af
Ross Hadden <rosshadden@gmail.com>
2013-06-06 20:23:20 -0400

    Added new map, and a ground object.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class playerController : MonoBehaviour {
 	CharacterController cc;
 	CollisionFlags cf;
 	
