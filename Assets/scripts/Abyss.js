2287123c3ca6df82a5a7084dc810e32be31616d1
Ross Hadden <rosshadden@gmail.com>
2013-08-01 00:11:03 -0400

    Added fall-death.

---
+++ Assets/scripts/Abyss.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+class Abyss extends MonoBehaviour {
+	function Start() {}
+	
+	function Update() {}
+	
+	function OnTriggerEnter(other : Collider) {
+		if (other.gameObject.tag == "Player") {
+			Destroy(other.gameObject);
+			Application.Quit();
+		}
+	}
+}
